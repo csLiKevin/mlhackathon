d12811b14642f20e8020d17e64b74e8fa68209c6
csLiKevin <cs.Li.Kevin@gmail.com>
2019-01-09 18:34:15 -0500

    Update ignored files.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { UploadButton } from './UploadButton.js';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            video: null
+        };
+
+        this.handleUploadVideo = this.handleUploadVideo.bind(this);
+    }
+
+    handleUploadVideo(event) {
+        console.log(event.dataTransfer.files);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <UploadButton onDropComplete={ this.handleUploadVideo } />
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/UploadButton.js
@@ -0,0 +1,71 @@
+import React, { createRef, Component } from "react";
+import Button from '@material-ui/core/Button';
+
+
+export class UploadButton extends Component {
+    constructor(props){
+        super(props);
+
+        // Reference to the hidden input element used to select local files.
+        this.inputField = createRef();
+
+        // Bind event handlers.
+        this.handleOnChange = this.handleOnChange.bind(this);
+        this.handleOnClick = this.handleOnClick.bind(this);
+        this.handleOnDrop = this.handleOnDrop.bind(this);
+    }
+
+    static defaultProps = {
+        onDropComplete: () => {}
+    };
+
+    handleOnChange() {
+        const event = new Event('drop');
+        event.dataTransfer = { files: this.inputField.current.files };
+        this.handleOnDrop(event);
+    }
+
+    handleOnClick() {
+        this.inputField.current.click();
+    }
+
+    handleOnDragEnter(event) {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+
+    handleOnDragLeave(event) {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+
+    handleOnDragOver(event) {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+
+    handleOnDrop(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        event.dataTransfer.dropEffect = 'copy';
+        this.props.onDropComplete(event);
+    }
+
+    render() {
+        return (
+            <Button
+                color="primary"
+                onClick={ this.handleOnClick }
+                onDragEnter={ this.handleOnDragEnter }
+                onDragLeave={ this.handleOnDragLeave }
+                onDragOver={ this.handleOnDragOver }
+                onDrop={ this.handleOnDrop }
+                variant="outlined"
+            >
+                <input hidden ref={ this.inputField } type='file' onChange={ this.handleOnChange }/>
+                Drag and Drop
+            </Button>
+        );
+    }
+}
+export default UploadButton;
